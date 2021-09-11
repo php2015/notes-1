67fdf47594e1ae2df6653d3cde50720d689cda27
clareliu <57534687+clareliu458@users.noreply.github.com>
2021-09-11 22:53:38 +0800

    Add files via upload

---
+++ vue/plugins/momentjs/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+  <h3>{{recordTime}}</h3>
+  <h3>{{now}}</h3>
+  </div>
+</template>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      now: moment().format("MMMM Do YYYY, h:mm:ss a"),
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.now = moment().format("MMMM Do YYYY, h:mm:ss a");
+    }, 1000);
+  },
+  components: {},
+  computed: {
+    recordTime() {
+      return moment().format("MMMM Do YYYY, h:mm:ss a");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
